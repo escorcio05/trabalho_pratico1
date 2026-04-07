7fb13aa7cb734c6d49e1ebac3298435470d808c6
alexandre-pereira-UA <alexandre.rp@ua.pt>
2026-04-07 12:10:11 +0100

    Again

--- tp1/tp1/tp1/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from app import views
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', views.lista_filmes, name='lista_filmes'),
-    path('filme/<int:filme_id>/', views.detalhe_filme, name='detalhe_filme'),
-]
